e7b85189b524937dcf731714f9252e51df409cd7
Adithya Kamath <adithyakamath96@gmail.com>
2024-08-02 02:14:07 +0530

    a deposit and withdrawal

--- JAVA/LombokLearnings/src/main/java/org/kamathad/BankAccount.java
+++ JAVA/LombokLearnings/src/main/java/org/kamathad/BankAccount.java
@@ -14,5 +14,17 @@ public class BankAccount {
         this.balance = balance;
     }
 
+    public void deposit(double amountToBeAdded){
+        this.balance += amountToBeAdded;
+        System.out.println("Balance successfuly updated. Current balance "+this.balance);
+    }
+
+    public void withdraw(double withdrawalAmount){
+        if(balance - withdrawalAmount < 0) {
+            System.out.println("You do not have sufficient balance to make a withdrawal");
+        }
+        this.balance -= withdrawalAmount;
+        System.out.println("Balance updated. Current balance "+this.balance);
+    }
 
 }
