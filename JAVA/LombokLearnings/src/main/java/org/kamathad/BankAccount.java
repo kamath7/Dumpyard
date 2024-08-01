5caadee37e5fcc533b31317540c7b5362b3a319b
Adithya Kamath <adithyakamath96@gmail.com>
2024-08-02 02:11:43 +0530

    creating a simple class

---
+++ JAVA/LombokLearnings/src/main/java/org/kamathad/BankAccount.java
@@ -0,0 +1,18 @@
+package org.kamathad;
+
+public class BankAccount {
+
+    private String accountNumber;
+
+    private String accountHolderName;
+
+    private double balance;
+
+    public BankAccount(String accountNumber, String accountHolderName, double balance) {
+        this.accountNumber = accountNumber;
+        this.accountHolderName = accountHolderName;
+        this.balance = balance;
+    }
+
+
+}
---
+++ JAVA/LombokLearnings/src/main/java/org/kamathad/Main.java
@@ -0,0 +1,7 @@
+package org.kamathad;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
